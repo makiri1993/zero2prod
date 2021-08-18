a0d4249a59608a08af8e7b500958794aec07fa6b
makiri1993 <kireew.martin@icloud.com>
2021-08-18 23:22:02 +0200

    remove unnecessary github action

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -9,11 +9,6 @@ pub struct TestApp {
     pub address: String,
     pub connection_pool: PgPool,
 }
-// [...]
-// No .await call, therefore no need for `spawn_app` to be async now.
-// We are also running tests, so it is not worth it to propagate errors:
-// if we fail to perform the required setup we can just panic and crash
-// all the things.
 async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
