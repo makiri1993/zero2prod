61d6002d751051c726a05a67a2cec8f13520a08e
makiri1993 <kireew.martin@icloud.com>
2021-08-18 21:05:34 +0200

    init zero2prod

---
+++ src/startup.rs
@@ -0,0 +1,23 @@
+use std::net::TcpListener;
+
+use crate::routes::{health_check::health_check, subscriptions::subscribe};
+
+use actix_web::{dev, web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+pub fn run(listener: TcpListener) -> Result<dev::Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
